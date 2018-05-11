9bf8cb81bea8aa3766caed6e84ea04a3559b245d
Govind Pimpale <gpimpale29@gmail.com>
2018-05-10 21:06:48 -0700

    made backups hierarchical

--- misc-init.el
+++ misc-init.el
@@ -1,6 +1,26 @@
+;; The directory to save all the backups in
 (defvar --backup-directory (concat user-emacs-directory "backups"))
 (if (not (file-exists-p --backup-directory))
-    (make-directory --backup-directory t))
+  (make-directory --backup-directory t))
+
+;; make backup to a designated dir, mirroring the full path
+
+(defun my-backup-file-name (fpath)
+  "Return a new file path of a given file path.
+  If the new path's directories does not exist, create them."
+  (let* (
+	 (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, for example, “C:”
+	 (backupFilePath (replace-regexp-in-string "//" "/" (concat --backup-directory filePath "~") ))
+	 )
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath
+    )
+  )
+
+(setq make-backup-file-name-function 'my-backup-file-name)
+
+
+
 (setq make-backup-directory-alist `(("." . ,--backup-directory)))
 (setq make-backup-files t
       backup-by-copying t
@@ -8,8 +28,11 @@
       delete-old-versions t
       auto-save-default t)
 
+
+
+
+
+
+
+
 (setq vc-follow-symlinks nil)
-
-
-
-
