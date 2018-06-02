d105ab5899ba4dfb591ff4f3c65866afbdbb23c1
Govind Pimpale <gpimpale29@gmail.com>
2018-06-02 11:59:59 -0700

    i3 is better than exwm sorry emacs

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (javadoc-lookup exwm-surf evil-surround exwm))))
+ '(package-selected-packages (quote (javadoc-lookup evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
